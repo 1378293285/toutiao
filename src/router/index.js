5d0e68fc53f3988f751b1b350b59d63c66e64dab
luog <1378293285@qq.com>
2019-08-05 17:52:14 +0800

    首页完成

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,20 @@ import VueRouter from 'vue-router'
 
 import Login from '@/views/login/index.vue'
 import Home from '@/views/home/index.vue'
+import Welcome from '@/views/welcome/index.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/login', name: 'login', component: Login },
-    { path: '/', name: 'home', component: Home }
+    { path: '/',
+      component: Home,
+      children: [
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+
+    }
   ]
 })
 
---
+++ src/views/welcome/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img src="../../assets/images/welcome.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  div {
+
+    img {
+      display: block;
+      margin: 0 auto;
+    }
+  }
+</style>
